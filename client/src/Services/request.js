637293d338c4ccad9a8e78ccfdfbdb2febf78297
Helmy-chaabane <Chaabane504@gmail.com>
2021-04-20 20:35:29 +0100

    adding requests

--- client/src/Services/request.js
+++ client/src/Services/request.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
-export const request = async () => {
+export default async (methode, url, id = "", body = null) => {
   try {
-    const { data } = await axios["get"]();
-    return data;
+    switch (methode) {
+      case "get":
+      case "delete":
+        return ({ data } = await axios[methode](url + id));
+
+      case "post":
+      case "put":
+        return ({ data } = await axios[methode](url + id, body));
+      default:
+        throw new Error("no sucha request !");
+    }
   } catch (error) {
     return error.message;
   }
