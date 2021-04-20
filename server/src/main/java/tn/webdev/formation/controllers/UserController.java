db0c11450d85b6e8b6bfc0974f9361c0139801e9
bahREEE <bhrmitn@gmail.com>
2021-04-20 17:45:50 +0100

    updated controller

--- server/src/main/java/tn/webdev/formation/controllers/UserController.java
+++ server/src/main/java/tn/webdev/formation/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,13 @@ public class UserController {
     public ResponseEntity<String> addRole(@RequestBody AppUser user){
         userRepository.save(user);
         return new ResponseEntity<>("User added successfully", HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/updateuser/{id}")
+    public ResponseEntity<String> updateuser(@PathVariable Long id, @RequestBody AppUser user){
+        user.setId(id);
+        userRepository.save(user);
+        return new ResponseEntity<>("User updated successfully !", HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/deleteallusers")
