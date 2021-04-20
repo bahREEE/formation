fd56f4c0728a446f754b43ed1d458da3b48ba2c2
Helmy-chaabane <Chaabane504@gmail.com>
2021-04-21 00:26:14 +0100

    form fix

---
+++ client/src/Components/Buttons/ResetButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResetButton = () => {
+  return <button></button>;
+};
+
+export default ResetButton;
--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import Input from "../Input/Input";
+import Select from "../Select/Select";
 import SubmitButton from "../Buttons/SubmitButton.jsx";
-import "../../Style/style.scss";
+import "../../Style/utilities.scss";
 import "./form.scss";
 
 const Form = () => {
   return (
-    <div className="div">
-      <form className="form" action="">
-        <Input required />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <div className="__submit">
+    <div className="form">
+      <h1>Add user</h1>
+      <form className="form__content">
+        <div className="mb-small">
+          <Input required />
+        </div>
+
+        <div className="mb-small">
+          <Input required />
+        </div>
+        <div className="mb-small">
+          <Select />
+        </div>
+
+        <div className="form__submit mt-meduim">
           <SubmitButton />
         </div>
       </form>
