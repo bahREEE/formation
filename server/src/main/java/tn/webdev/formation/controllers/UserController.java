a7c4c17a7e410e2adabcc821ba3b74b09914a4c6
bahREEE <bhrmitn@gmail.com>
2021-04-20 19:50:09 +0100

    add user

--- server/src/main/java/tn/webdev/formation/controllers/UserController.java
+++ server/src/main/java/tn/webdev/formation/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,9 @@ import tn.webdev.formation.entities.ERole;
 @RestController
 public class UserController {
     
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private UserRepository userRepository;
 
@@ -42,6 +46,7 @@ public class UserController {
     @PostMapping(value = "/addUser")
     public ResponseEntity<String> addRole(@RequestBody AppUser user){
         addRoleToUser(user,"ADMINISTRATEUR");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return new ResponseEntity<>("User added successfully", HttpStatus.OK);
     }
